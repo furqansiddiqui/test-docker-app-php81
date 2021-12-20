8176ce802dd6ade3c7b2412c65e52df5c4fd0b3a
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-12-20 22:41:30 +0500

    DataStore

---
+++ app/common/DataStore/AbstractDataStoreObject.php
@@ -0,0 +1,173 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Common\DataStore;
+
+use App\Common\AppKernel;
+use App\Common\Database\Primary\DataStore;
+use App\Common\Exception\AppException;
+use Comely\Buffer\Buffer;
+use Comely\Security\Exception\CipherException;
+
+/**
+ * Class AbstractDataStoreObject
+ * @property int $cachedOn
+ * @method void beforeSave()
+ * @package App\Common\DataStore
+ */
+abstract class AbstractDataStoreObject
+{
+    /** @var array */
+    private static array $instances = [];
+
+    /** @var null|string */
+    public const DB_KEY = null;
+    /** @var null|string */
+    public const CACHE_KEY = null;
+    /** @var null|int */
+    public const CACHE_TTL = null;
+    /** @var bool */
+    public const IS_ENCRYPTED = false;
+
+    /**
+     * @param bool $useCache
+     * @return static
+     * @throws AppException
+     */
+    public static function getInstance(bool $useCache = true): self
+    {
+        $instanceId = static::class;
+        if (isset(static::$instances[$instanceId])) {
+            return static::$instances[$instanceId];
+        }
+
+        $aK = AppKernel::getInstance();
+
+        if ($useCache && is_string(static::CACHE_KEY)) {
+            try {
+                $cache = $aK->cache;
+                $dataStoreObject = $cache->get(static::CACHE_KEY);
+            } catch (\Exception) {
+            }
+        }
+
+        if (isset($dataStoreObject) && $dataStoreObject instanceof static) {
+            static::$instances[$instanceId] = $dataStoreObject;
+            return static::$instances[$instanceId];
+        }
+
+        if (!is_string(static::DB_KEY)) {
+            throw new \UnexpectedValueException(sprintf('Invalid DataStore object "%s" DB key', $instanceId));
+        }
+
+        // Fetch from database
+        try {
+            $db = $aK->db->primary();
+            $query = $db->query()->table(DataStore::NAME)
+                ->where('`key`=?', [static::DB_KEY])
+                ->limit(1)
+                ->fetch()
+                ->row();
+
+            if (!$query || !isset($query["data"]) || !is_string($query["data"])) {
+                throw new AppException(sprintf('DataStore object "%s" not found in DB', static::DB_KEY));
+            }
+
+            $bytes = new Buffer($query["data"]);
+            $dataStoreObject = static::IS_ENCRYPTED ?
+                $aK->ciphers->project()->decrypt($bytes) : unserialize($bytes->raw());
+
+            if (!$dataStoreObject instanceof static) {
+                throw new AppException(sprintf('Failed to unserialize "%s" DataStore object', static::DB_KEY));
+            }
+        } catch (AppException $e) {
+            throw $e;
+        } catch (\Exception $e) {
+            $aK->errors->triggerIfDebug($e, E_USER_WARNING);
+            throw new AppException(sprintf('Failed to retrieve "%s" DataStore object', static::DB_KEY));
+        }
+
+        // Store in cache?
+        if (isset($cache)) {
+            try {
+                $clonedObject = clone $dataStoreObject;
+                $clonedObject->cachedOn = time();
+                $cache->set(static::CACHE_KEY, $clonedObject, static::CACHE_TTL);
+            } catch (\Exception $e) {
+                $aK->errors->triggerIfDebug($e, E_USER_WARNING);
+                trigger_error(sprintf('Failed to store "%s" DataStore object in cache', static::DB_KEY), E_USER_WARNING);
+            }
+        }
+
+        static::$instances[$instanceId] = $dataStoreObject;
+        return static::$instances[$instanceId];
+    }
+
+    /**
+     * @return array
+     */
+    public function __debugInfo(): array
+    {
+        return [static::class];
+    }
+
+    /**
+     * Use this method to set/change property values, returns TRUE if new value is different from previous one
+     * @param string $prop
+     * @param $val
+     * @param bool $checkUnInitProp
+     * @return bool
+     * @throws AppException
+     */
+    public function setValue(string $prop, $val, bool $checkUnInitProp = false): bool
+    {
+        if (!property_exists($this, $prop)) {
+            throw new AppException(sprintf('Prop "%s" does not exist in class "%s"', $prop, static::class));
+        }
+
+        if ($checkUnInitProp) {
+            $change = true;
+            if (isset($this->$prop)) {
+                $change = $this->$prop !== $val;
+            }
+
+            if ($change) {
+                $this->$prop = $val;
+            }
+
+            return $change;
+        }
+
+        if ($this->$prop !== $val) {
+            $this->$prop = $val;
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return void
+     * @throws AppException
+     * @throws \Comely\Database\Exception\DbConnectionException
+     * @throws \Comely\Database\Exception\QueryExecuteException
+     */
+    public function save(): void
+    {
+        if (static::IS_ENCRYPTED) {
+            try {
+                if (method_exists($this, 'beforeSave')) {
+                    $this->beforeSave();
+                }
+
+                $bytes = AppKernel::getInstance()->ciphers->project()->encrypt($this);
+            } catch (CipherException) {
+                throw new AppException(sprintf('Failed to encrypt "%s" DataStore object', static::class));
+            }
+        } else {
+            $bytes = new Buffer(serialize($this));
+        }
+
+        DataStore::Save(static::DB_KEY, $bytes);
+    }
+}
---
+++ app/common/Database/Primary/DataStore.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Common\Database\Primary;
+
+use App\Common\AppKernel;
+use App\Common\Database\AbstractAppTable;
+use App\Common\Validator;
+use Comely\Buffer\Buffer;
+use Comely\Database\Schema\Table\Columns;
+use Comely\Database\Schema\Table\Constraints;
+
+/**
+ * Class DataStore
+ * @package App\Common\Database\Primary
+ */
+class DataStore extends AbstractAppTable
+{
+    public const NAME = 'd_storage';
+    public const MODEL = null;
+    public const DATA_MAX_BYTES = 10240; // 10 KiB
+
+    /**
+     * @param Columns $cols
+     * @param Constraints $constraints
+     * @return void
+     */
+    public function structure(Columns $cols, Constraints $constraints): void
+    {
+        $cols->defaults("ascii", "ascii_general_ci");
+
+        $cols->string("key")->length(40)->unique();
+        $cols->binary("data")->length(self::DATA_MAX_BYTES);
+        $cols->string("raw")->length(255)->nullable();
+        $cols->int("time_stamp")->bytes(4)->unSigned();
+    }
+
+    /**
+     * @param string $key
+     * @param Buffer $data
+     * @param string|null $raw
+     * @return void
+     * @throws \Comely\Database\Exception\DbConnectionException
+     * @throws \Comely\Database\Exception\QueryExecuteException
+     */
+    public static function Save(string $key, Buffer $data, ?string $raw = null): void
+    {
+        if (!Validator::isASCII($key, "-.+@")) {
+            throw new \InvalidArgumentException('Invalid DataStore object key');
+        } elseif ($data->len() > self::DATA_MAX_BYTES) {
+            throw new \LengthException(
+                sprintf('Length of DataStore object "%s" cannot exceed %d bytes', $key, self::DATA_MAX_BYTES)
+            );
+        }
+
+        $aK = AppKernel::getInstance();
+        $query = 'INSERT ' . 'INTO `%s` (`key`, `data`, `raw`, `time_stamp`) VALUES (:key, :data, :raw, :timeStamp) ' .
+            'ON DUPLICATE KEY UPDATE `data`=:data, `raw`=:raw, `time_stamp`=:timeStamp';
+        $queryData = [
+            "key" => $key,
+            "data" => $data->raw(),
+            "raw" => $raw,
+            "timeStamp" => time()
+        ];
+
+        $aK->db->primary()->exec(sprintf($query, self::NAME), $queryData);
+    }
+}
