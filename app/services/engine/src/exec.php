b5b139e4b67fc02f0f858052a761670c4f7b4862
Furqan Siddiqui <hello@furqansiddiqui.com>
2022-01-05 02:49:58 +0500

    init engine

---
+++ app/services/engine/src/exec.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+chdir(__DIR__); // Change PHP current working directory
+
+/** @noinspection PhpIncludeInspection */
+require "../vendor/autoload.php";
+
+try {
+    $aK = \App\Common\AppKernel::Bootstrap();
+
+    // Prepare Arguments
+    $args = $argv[1] ?? "";
+    $args = explode(";", substr($args, 1, -1));
+
+    // Instantiate CLI
+    $bin = new \Comely\Filesystem\Directory(__DIR__ . DIRECTORY_SEPARATOR . "scripts");
+    $cli = new \App\Common\Kernel\CLI($aK, $bin, $args);
+
+    // Listen to events, etc...
+    $cli->events->beforeExec();
+
+    // Execute
+    $cli->exec();
+} catch (Exception $e) {
+    /** @noinspection PhpUnhandledExceptionInspection */
+    throw $e;
+}
