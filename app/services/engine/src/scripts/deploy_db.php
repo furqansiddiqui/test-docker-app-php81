b7e5296bd394f8f1fda7415646981a99165e0632
Furqan Siddiqui <hello@furqansiddiqui.com>
2022-01-07 02:03:11 +0500

    installer script

--- app/services/engine/src/scripts/deploy_db.php
+++ app/services/engine/src/scripts/deploy_db.php
@@ -3,15 +3,12 @@ declare(strict_types=1);
 
 namespace bin;
 
-use App\Common\Kernel\CLI\AbstractCLIScript;
-use Comely\Database\Database;
-use Comely\Database\Schema;
 
 /**
  * Class deploy_db
  * @package bin
  */
-class deploy_db extends AbstractCLIScript
+class deploy_db extends abstract_db_builder_script
 {
     /**
      * @return void
@@ -21,77 +18,6 @@ class deploy_db extends AbstractCLIScript
      */
     public function exec(): void
     {
-        // Primary Database
-        $this->inline("Getting {invert}{yellow} primary {/} database ... ");
-        $primary = $this->aK->db->primary();
-        $this->print("{green}OK{/}");
-
-        $this->createDbTables($primary, [
-            'App\Common\Database\Primary\Administrators',
-            'App\Common\Database\Primary\Admin\Logs',
-            'App\Common\Database\Primary\Admin\Sessions',
-            'App\Common\Database\Primary\DataStore',
-            'App\Common\Database\Primary\ProcessTracker',
-        ]);
-
-        $this->print("");
-
-        // API Logs Database
-        $this->inline("Getting {invert}{yellow} API Logs {/} database ... ");
-        $apiLogs = $this->aK->db->apiLogs();
-        $this->print("{green}OK{/}");
-
-        $this->createDbTables($apiLogs, [
-        ]);
-
-        $this->print("");
-    }
-
-    /**
-     * @param Database $db
-     * @param array $tables
-     * @return void
-     * @throws \Comely\Database\Exception\QueryExecuteException
-     * @throws \Comely\Database\Exception\SchemaTableException
-     */
-    private function createDbTables(Database $db, array $tables): void
-    {
-        $loaded = [];
-
-        $this->print("{grey}Fetching tables... {/}");
-        foreach ($tables as $tableClass) {
-            $this->inline(sprintf('{cyan}%s{/} ... ', $tableClass));
-            $tableName = constant(sprintf('%s::TABLE', $tableClass));
-            Schema::Bind($db, $tableClass);
-            $loaded[] = $tableName;
-            $this->print("{green}OK{/}");
-        }
-
-
-        $this->print("");
-        $this->print("{grey}Building database tables...{/}");
-        foreach ($loaded as $table) {
-            $this->inline(sprintf('CREATE' . ' TABLE IF NOT EXISTS `{cyan}%s{/}` ... ', $table));
-            $migration = Schema::Migration($table)->createIfNotExists()->createTable();
-
-            $db->exec($migration);
-            $this->print('{green}SUCCESS{/}');
-        }
-    }
-
-    /**
-     * @return string|null
-     */
-    public function processInstanceId(): ?string
-    {
-        return null;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function semaphoreLockId(): ?string
-    {
-        return null;
+        $this->createDbTables();
     }
 }
---
+++ app/services/engine/src/scripts/install.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types=1);
+
+namespace bin;
+
+use App\Common\Admin\Administrator;
+use App\Common\Admin\Credentials;
+use App\Common\Admin\Privileges;
+use App\Common\AppConstants;
+use App\Common\Database\Primary\Administrators;
+use App\Common\Validator;
+use Comely\Database\Exception\ORM_ModelNotFoundException;
+use Comely\Utils\ASCII;
+
+/**
+ * Class install
+ * @package bin
+ */
+class install extends abstract_db_builder_script
+{
+    /**
+     * @return void
+     * @throws \App\Common\Exception\AppException
+     * @throws \Comely\Database\Exception\DatabaseException
+     * @throws \Comely\Security\Exception\CipherException
+     */
+    public function exec(): void
+    {
+        // Create database tables
+        $this->createDbTables();
+        $this->print("");
+
+        // Check if administrator account exists
+        try {
+            $admins = Administrators::Find()->limit(1)->all();
+        } catch (ORM_ModelNotFoundException) {
+            $admins = [];
+        }
+
+        if ($admins) { // Already exists?
+            $this->print("{yellow}Administration accounts already exists!");
+            $this->print("{grey}Skip installation process...");
+            return;
+        }
+
+        // Create admin account
+        $this->print("Creating an administration account...");
+        $adminEmail = null;
+        $adminPassword = null;
+        while (!$adminEmail) {
+            $inputEmail = trim(strval($this->requireInput("{yellow}Administrator E-mail Address?{/} {cyan}")));
+            if (Validator::isValidEmailAddress($inputEmail)) {
+                $adminEmail = $inputEmail;
+                break;
+            }
+
+            $this->print("{red}Invalid e-mail address")->eol();
+        }
+
+        while (!$adminPassword) {
+            $inputPassword = trim(strval($this->requireInput("{yellow}Password:{/} {cyan}")));
+            $pwLen = strlen($inputPassword);
+            try {
+                if (!ASCII::isPrintableOnly($inputPassword)) {
+                    throw new \RuntimeException('Password contains an invalid character');
+                } elseif ($pwLen < 8 || $pwLen > 32) {
+                    throw new \RuntimeException('Password must be between 8 to 32 characters log');
+                }
+            } catch (\RuntimeException $e) {
+                $this->print("{red}" . $e->getMessage())->eol();
+                continue;
+            }
+
+            $adminPassword = $inputPassword;
+            break;
+        }
+
+        // Admin account
+        $admin = new Administrator();
+        $admin->id = AppConstants::ROOT_ADMINISTRATORS[0];
+        $admin->status = 1;
+        $admin->email = $adminEmail;
+        $admin->timeStamp = time();
+
+        $credentials = new Credentials($admin);
+        $credentials->changePassword($adminPassword);
+        $privileges = new Privileges($admin);
+
+        $admin->set("credentials", $admin->cipher()->encrypt($credentials)->raw());
+        $admin->set("privileges", $admin->cipher()->encrypt($privileges)->raw());
+        $admin->set("checksum", $admin->checksum()->raw());
+        $admin->query()->insert();
+
+        $this->print("");
+        $this->print("{grey}Administrator account has been created!");
+        $this->print("{green}Installation finished!");
+    }
+}
