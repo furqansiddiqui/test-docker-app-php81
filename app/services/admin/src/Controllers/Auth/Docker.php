876bd52f9c3f009201fbbe03155e3cf21df1358d
Furqan Siddiqui <hello@furqansiddiqui.com>
2022-01-10 10:12:32 +0500

    updates

--- app/services/admin/src/Controllers/Auth/Docker.php
+++ app/services/admin/src/Controllers/Auth/Docker.php
@@ -20,6 +20,17 @@ class Docker extends AbstractAdminAPIController
     }
 
     /**
+     * @param string $ipAddress
+     * @param int $n
+     * @return bool
+     */
+    private function pingService(string $ipAddress, int $n = 3): bool
+    {
+        exec(sprintf("ping -n %d %s", $n, $ipAddress), result_code: $resultCode);
+        return $resultCode === 0;
+    }
+
+    /**
      * @return void
      * @throws AdminAPIException
      * @throws \App\Common\Exception\DockerConfigException
@@ -28,8 +39,52 @@ class Docker extends AbstractAdminAPIController
      */
     protected function get(): void
     {
+        $dockerConfig = DockerConfig::getInstance($this->aK, useCache: true);
+
+        // Specific service info requested?
+        $serviceId = strtolower($this->input()->getASCII("service"));
+        if ($serviceId) {
+            $dockerService = null;
+            /** @var DockerConfig\DockerConfigService $service */
+            foreach ($dockerConfig->services as $service) {
+                if (strtolower($service->id) === $serviceId) {
+                    $dockerService = $service;
+                    break;
+                }
+            }
+
+            if (!$dockerService) {
+                throw new AdminAPIException('Requested docker service does not exist');
+            }
+
+            // PING for status
+            if ($service->ipAddress) {
+                $service->status = $this->pingService($service->ipAddress, 2);
+            }
+
+            // JSON encode
+            try {
+                $dockerService = Validator::JSON_Filter($dockerService);
+            } catch (\JsonException) {
+                throw new AdminAPIException('Failed to encode DockerConfigService object');
+            }
+
+            $this->status(true);
+            $this->response->set("service", $dockerService);
+            return;
+        }
+
+        // PING all services
+        /** @var DockerConfig\DockerConfigService $service */
+        foreach ($dockerConfig->services as $service) {
+            if ($service->ipAddress) {
+                $service->status = $this->pingService($service->ipAddress, 2);
+            }
+        }
+
+        // JSON encode
         try {
-            $dockerConfig = Validator::JSON_Filter(DockerConfig::getInstance($this->aK, useCache: true));
+            $dockerConfig = Validator::JSON_Filter($dockerConfig);
         } catch (\JsonException) {
             throw new AdminAPIException('Failed to encode DockerConfig object');
         }
