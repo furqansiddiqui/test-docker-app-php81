101244ed657d71fd48d2871cae70ef78c5498ae3
Furqan Siddiqui <hello@furqansiddiqui.com>
2022-05-19 21:45:08 +0500

    correction

--- app/services/admin/src/Controllers/Auth/Staff/Insert.php
+++ app/services/admin/src/Controllers/Auth/Staff/Insert.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Services\Admin\Controllers\Auth\Staff\Insert;
+namespace App\Services\Admin\Controllers\Auth\Staff;
 
 use App\Common\Admin\Administrator;
 use App\Common\Admin\Credentials;
